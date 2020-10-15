e9ad345bb0ff1fe163e64114e0ae345b0df5b4fe
Jameson Midgley <jameson.midgley@gmail.com>
2020-10-15 15:32:17 -0600

    imporoving service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,15 +6,15 @@ const FILES_TO_CACHE = [
     "/manifest.json",
    ];
    
-   const CACHE_NAME = "static-cache-v2";
-   const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
    
-   self.addEventListener('install', (event) => {
-       event.waitUntil(
-         caches.open(DATA_CACHE_NAME).then(cache => {
-            console.log("cache installing");
-           return cache.addAll(FILES_TO_CACHE)
-       })
-     );
-     self.skipWaiting();
-   });
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+        console.log("cache installing");
+        return cache.addAll(FILES_TO_CACHE)
+    })
+    );
+    self.skipWaiting();
+});
