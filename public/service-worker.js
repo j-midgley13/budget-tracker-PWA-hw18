aab3d2194865a4f7c036df273c5227e6baf5f515
Jameson Midgley <jameson.midgley@gmail.com>
2020-10-15 15:13:48 -0600

    added service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "/manifest.json",
+   ];
+   
+   const CACHE_NAME = "static-cache-v2";
+   const DATA_CACHE_NAME = "data-cache-v1";
+   
+   self.addEventListener('install', (event) => {
+       event.waitUntil(
+         caches.open(DATA_CACHE_NAME).then(cache => {
+            console.log("cache installing");
+           return cache.addAll(FILES_TO_CACHE)
+       })
+     );
+     self.skipWaiting();
+   });
