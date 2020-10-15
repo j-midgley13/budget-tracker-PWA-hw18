78c6436302b6b51ac4fde76d3dd15ac9eec25d3a
Jameson Midgley <jameson.midgley@gmail.com>
2020-10-15 16:01:30 -0600

    added fetch request

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,10 +4,12 @@ const FILES_TO_CACHE = [
     "/styles.css",
     "/index.js",
     "/manifest.json",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
    ];
    
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+let CACHE_NAME = "static-cache-v2";
+let DATA_CACHE_NAME = "data-cache-v1";
    
 self.addEventListener('install', (event) => {
     event.waitUntil(
@@ -17,4 +19,38 @@ self.addEventListener('install', (event) => {
     })
     );
     self.skipWaiting();
+});
+
+self.addEventListener("fetch", function(event) {
+    // cache successful requests to the API
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(error => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        }).catch(error => console.log(error))
+      );
+  
+      return;
+    }
+  
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+    event.respondWith(
+        fetch(event.request).catch(function(){
+            return caches.match(event.request).then(function(response) {
+                return response;
+            })
+        })
+    );
 });
