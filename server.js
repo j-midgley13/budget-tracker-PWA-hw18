ec9e4e6b44f05a799791feafb0c077da8de5b93f
Jameson Midgley <jameson.midgley@gmail.com>
2020-10-20 18:39:54 -0600

    put mongodb back online

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  // process.env.MONGODB_URI || 
+  process.env.MONGODB_URI || 
   "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useCreateIndex: true,
