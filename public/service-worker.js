96701a0391f455b39becb1c8df5e411db589aa10
Jameson Midgley <jameson.midgley@gmail.com>
2020-10-20 18:36:05 -0600

    trying to fix heroku issue

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,6 @@
+let CACHE_NAME = "static-cache-v2";
+let DATA_CACHE_NAME = "data-cache-v1";
+
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
@@ -8,9 +11,6 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
    ];
-   
-let CACHE_NAME = "static-cache-v2";
-let DATA_CACHE_NAME = "data-cache-v1";
    
 self.addEventListener('install', (event) => {
     event.waitUntil(
